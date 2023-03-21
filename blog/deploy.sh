d3cbe2d5026116e338b5a33fc5a6867b08fb896e
Yiwei Zhao <zyw@Yiweis-MacBook-Pro.local>
2023-03-21 12:34:33 -0700

    remove hugo workflow

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-blog-awesome # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
@@ -18,7 +18,7 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push
 
 # Come Back up to the Project Root
 cd ..
